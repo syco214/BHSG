f8f71069a10016fbfabba9c3c4ab0211e08e3ea7
Sage Co <cojavis1@gmail.com>
2021-10-25 02:11:58 +0800

    commit 29

--- src/Carousel/index.js
+++ src/Carousel/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { useState } from "react";
 import Slider from "react-slick";
 import one from "../Images/1.png";
+import two from "../Images/2.JPG";
 import three from "../Images/3.png";
 import four from "../Images/4.png";
 import five from "../Images/5.png";
@@ -40,7 +41,7 @@ export const HeroContainer = styled.div`
         height: 300px;
     }
 `
-const images = [one, three, four, five, six, seven, eight, nine, ten, eleven, twelve];
+const images = [one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve];
 
 function Carousel() {
 
